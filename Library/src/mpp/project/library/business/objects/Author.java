6c7c636292e338b65c279c7fb23de9a82e18806d
jgsadang <jgsadang3@gmail.com>
2015-06-29 18:24:54 -0500

    1

---
+++ Library/src/mpp/project/library/business/objects/Author.java
@@ -0,0 +1,14 @@
+package mpp.project.library.business.objects;
+
+public class Author extends Person {
+	private String credentials;
+
+	public String getCredentials() {
+		return credentials;
+	}
+
+	public void setCredentials(String credentials) {
+		this.credentials = credentials;
+	}
+
+}
---
+++ Library/src/mpp/project/library/business/objects/LibraryMember.java
@@ -0,0 +1,6 @@
+package mpp.project.library.business.objects;
+
+public class LibraryMember extends Person {
+	private String memberID;
+	
+}
