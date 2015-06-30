ca9e9a76de78bf20b0c1a1f0b4edf59ec2c7adef
jgsadang <jgsadang3@gmail.com>
2015-06-30 11:32:52 -0500

    1

---
+++ Library/src/view/AddLibraryMember.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class AddLibraryMember extends VBox {
+	
+	public AddLibraryMember() {
+		Text text = new Text("AddLibraryMember");
+		this.getChildren().add(text);
+	}
+	
+}
---
+++ Library/src/view/SearchPublication.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class SearchPublication extends VBox {
+	
+	public SearchPublication() {
+		Text text = new Text("SearchPublication");
+		this.getChildren().add(text);
+	}
+	
+}
