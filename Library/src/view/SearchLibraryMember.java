e7668aa418ecf4cc5159f3a0a93f9df729ea0eac
984703 <984703@cs_labv32-110.cs.mum.edu>
2015-06-30 17:29:37 -0500

    search member ui

--- Library/src/view/SearchLibraryMember.java
+++ Library/src/view/SearchLibraryMember.java
@@ -1,13 +1,21 @@
 package view;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class SearchLibraryMember extends VBox {
 	
 	public SearchLibraryMember() {
-		Text text = new Text("SearchLibraryMember");
-		this.getChildren().add(text);
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("SearchLibraryMember.fxml"));
+			this.getChildren().add(root);
+		} catch (IOException io) {
+			System.out.println(io.getStackTrace());
+		}	
 	}
 	
 }
