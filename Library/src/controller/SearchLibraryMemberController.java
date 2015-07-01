945c00fa96ec07fe670909a18837f542bf47b766
jgsadang <jgsadang3@gmail.com>
2015-07-02 03:03:23 +0800

    updated SearchLibraryMemberController

--- Library/src/controller/SearchLibraryMemberController.java
+++ Library/src/controller/SearchLibraryMemberController.java
@@ -2,7 +2,10 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import business.dataaccess.DataAccess;
 import business.dataaccess.DataAccessFacade;
 import business.objects.LibraryMember;
@@ -21,6 +24,26 @@ public class SearchLibraryMemberController {
 	@FXML private TextField tfPhone;
 	
 	@FXML protected void handleSearchMemberBtnAction(ActionEvent event) {
+		
+		LibraryMember member = dao.searchLibraryMemberByID(Integer.parseInt(tfMemberID.getText()));
+		if (member != null) {
+			tfMemberID.setText(""+member.getMemberID());
+			tfFirstName.setText(member.getFirstName());
+			tfLastName.setText(member.getLastName());
+			tfPhone.setText(member.getPhone());
+			Address address = member.getAddress();
+			if (address!=null) {
+				tfStreet.setText(address.getStreet());
+				tfCity.setText(address.getCity());
+				tfState.setText(address.getState());
+				tfZip.setText(address.getZip());
+			}
+		} else {
+			Alert alert = new Alert(AlertType.ERROR, "Cannot find member id " + tfMemberID.getText(), ButtonType.OK);
+			alert.setTitle("Search Library Member");
+			alert.setContentText("Error");
+			alert.show();
+		}
 		
 
 	}
