4f27183a0f4809de6f4237ae5755f60f23f5e558
jgsadang <jgsadang3@gmail.com>
2015-07-01 10:52:31 +0800

    Update SearchPublication

--- Library/src/view/SearchPublication.java
+++ Library/src/view/SearchPublication.java
@@ -1,13 +1,21 @@
 package view;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class SearchPublication extends VBox {
 	
 	public SearchPublication() {
-		Text text = new Text("SearchPublication");
-		this.getChildren().add(text);
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("SearchPublication.fxml"));
+			this.getChildren().add(root);
+		} catch (IOException io) {
+			System.out.println(io.getStackTrace());
+		}	
 	}
 	
 }
