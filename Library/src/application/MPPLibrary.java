7ac8886843bba5eafb326b5782e10c4ea03988a1
jgsadang <jgsadang3@gmail.com>
2015-07-08 00:31:08 +0800

    updated css

--- Library/src/application/MPPLibrary.java
+++ Library/src/application/MPPLibrary.java
@@ -15,7 +15,7 @@ public class MPPLibrary extends Application {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("../view/LibraryMainView.fxml"));
 			Scene scene = new Scene(root,800,600);			
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
 			primaryStage.setTitle("MPP Library System");
 			primaryStage.setResizable(true);
 			primaryStage.setScene(scene);
