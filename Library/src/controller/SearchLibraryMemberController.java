07e5de5932d86396f42fd84675d191ff476ffffa
jgsadang <jgsadang3@gmail.com>
2015-07-13 04:03:30 +0800

    updated and added checkout record table for use case 2

--- Library/src/controller/SearchLibraryMemberController.java
+++ Library/src/controller/SearchLibraryMemberController.java
@@ -1,16 +1,40 @@
 package controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import table.objects.CheckoutRecordTable;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 import business.dataaccess.DataAccess;
 import business.dataaccess.DataAccessFacade;
+import business.objects.Book;
+import business.objects.CheckoutRecordEntry;
 import business.objects.LibraryMember;
 import business.objects.Address;
+import business.objects.Periodical;
+import business.objects.Publication;
 
 public class SearchLibraryMemberController {
 	private final DataAccess dao = DataAccessFacade.getDAO();
@@ -25,6 +49,7 @@ public class SearchLibraryMemberController {
 	@FXML private TextField tfZip;
 	@FXML private TextField tfPhone;
 	@FXML private Button btnUpdate;
+	@FXML private HBox hbSearchResult;
 	
 	@FXML protected void handleSearchMemberBtnAction(ActionEvent event) {
 
@@ -57,6 +82,7 @@ public class SearchLibraryMemberController {
 			}
 			
 			btnUpdate.setDisable(false);
+			processShowCheckoutRecord();
 		} else {
 			this.memberId = 0;
 			
@@ -109,6 +135,130 @@ public class SearchLibraryMemberController {
 			alert.setTitle("Update Library Member");
 			alert.show();
 		} 
+	}
+	
+	private void processShowCheckoutRecord() {
+		   List<CheckoutRecordEntry> checkoutRecord = dao.getCheckoutRecordEntryByMemberID( memberId );
+		   ArrayList<CheckoutRecordTable> list = new ArrayList<CheckoutRecordTable>();
+		   if (checkoutRecord != null) {		 
+			   for(CheckoutRecordEntry ce: checkoutRecord){
+				   CheckoutRecordTable chkRec = new CheckoutRecordTable();
+				   chkRec.setTitle(ce.getCopy().getPublication().getTitle());
+				   chkRec.setNumber(ce.getCopy().getPublication().getNumber());
+				   chkRec.setBorrowedDate(ce.getCheckoutDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
+				   chkRec.setDueDate(ce.getDueDate().format(DateTimeFormatter.ISO_LOCAL_DATE));
+				   chkRec.setCopyNum(ce.getCopy().getCopyNo());
+				   if(ce.getDueDate().isBefore(LocalDate.now())){
+					   //Period betweenDates = Period.between(ce.getDueDate(), LocalDate.now());
+					   long dayDiff = ChronoUnit.DAYS.between(ce.getDueDate(), LocalDate.now());
+					   chkRec.setStatus(dayDiff + " day(s) overdue");
+				   } else {
+					   chkRec.setStatus("");
+				   }
+				   if (ce.getCopy().getPublication() instanceof Book) {
+					   chkRec.setType("book");
+				   } else {
+					   chkRec.setType("periodical");
+				   }
+				   list.add(chkRec);
+			   }
+		   }
+		   if (hbSearchResult.getChildren().size() == 1) {
+				hbSearchResult.getChildren().remove(0);
+		   }
+			hbSearchResult.getChildren().add(getCheckoutTable(list));
+	}
+	
+	private TableView getCheckoutTable(ArrayList<CheckoutRecordTable> list) {
+		TableView table = new TableView();
+		table.setPrefWidth(600);
+
+		TableColumn colTitle = new TableColumn("Title");
+		colTitle.setPrefWidth(125);
+		colTitle.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("title"));
+		
+		TableColumn colISBN = new TableColumn("ISBN/Issue #");
+		colISBN.setPrefWidth(80);
+		colISBN.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("number"));
+		
+		TableColumn colCopy = new TableColumn("Copy Number");
+		colCopy.setPrefWidth(55);
+		colCopy.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("copyNum"));
+
+		TableColumn colType = new TableColumn("Type");
+		colType.setPrefWidth(60);
+		colType.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("type"));
+		
+		TableColumn colBorrowedDate = new TableColumn("Borrowed");
+		colBorrowedDate.setPrefWidth(80);
+		colBorrowedDate.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("borrowedDate"));
+		
+		TableColumn colDueDate = new TableColumn("Due");
+		colDueDate.setPrefWidth(80);
+		colDueDate.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("dueDate"));	
+		
+		TableColumn colStatus = new TableColumn("Status");
+		colStatus.setPrefWidth(100);
+		colStatus.setCellValueFactory(
+                new PropertyValueFactory<CheckoutRecordTable, String>("status"));					
+		
+		table.setRowFactory(
+			    new Callback<TableView<CheckoutRecordTable>, TableRow<CheckoutRecordTable>>() {
+			    	
+			  @Override
+			  public TableRow<CheckoutRecordTable> call(TableView<CheckoutRecordTable> tableView) {
+			    final TableRow<CheckoutRecordTable> row = new TableRow<>();
+			    final ContextMenu rowMenu = new ContextMenu();
+			    MenuItem checkoutItem = new MenuItem("Return");
+			    checkoutItem.setOnAction(new EventHandler<ActionEvent>() {
+			      @Override
+			      public void handle(ActionEvent event) {
+			    	  Alert alert = new Alert(AlertType.CONFIRMATION);
+			    	  alert.setContentText("Confirm return item.");
+			    	  
+			    	  Optional<ButtonType> result = alert.showAndWait();
+			    	  if (result.isPresent() && result.get() == ButtonType.OK) {
+			    		  int memberId = Integer.parseInt(tfMemberID.getText());
+			    		  CheckoutRecordEntry checkoutRecordEntry = dao.getCheckoutRecordEntryById(memberId, 
+			    				  row.getItem().getType(), row.getItem().getNumber(), row.getItem().getCopyNum());
+			    		  
+			    		  Publication pub = checkoutRecordEntry.getCopy().getPublication();
+			    		  dao.removeCheckoutRecordEntry(memberId, pub, checkoutRecordEntry.getCopy());
+
+			    		  pub.returnACopy();
+			    		  if(pub.getClass().getName().contains("Book")){
+			    			  dao.saveUpdateBook( (Book)pub );
+			    		  } else {
+			    			  dao.saveUpdatePeriodical( (Periodical)pub );
+			    		  }
+			    		  
+			    		  row.setPrefHeight(0);
+			    	  }
+			      }
+			    });
+			    rowMenu.getItems().addAll(checkoutItem);
+
+			    // only display context menu for non-null items:
+			    row.contextMenuProperty().bind(
+			      Bindings.when(Bindings.isNotNull(row.itemProperty()))
+			      .then(rowMenu)
+			      .otherwise((ContextMenu)null));
+			    return row;
+			  }
+			});		
+		
+		ObservableList<CheckoutRecordTable> data = FXCollections.observableArrayList();
+		data.addAll(list);
+		table.setItems(data);
+		table.getColumns().addAll(colTitle, colType, colISBN, colCopy,colBorrowedDate, colDueDate, colStatus);
+		
+		return table;
 	}
 }
 
