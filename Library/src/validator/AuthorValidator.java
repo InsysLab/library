5a1832a919b1abadb5cc09ad679b2ed6f04a44c0
boldhuu <d.boldkhuu@gmail.com>
2015-07-12 22:42:34 -0500

    fixed

--- Library/src/validator/AuthorValidator.java
+++ Library/src/validator/AuthorValidator.java
@@ -1,31 +1,28 @@
 package validator;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
 public final class AuthorValidator {
 	public static String validateAuthor(String bio, String fName, String lname, String phone) {
-		return  firstName(fName) + lastName(lname) + phone(phone);		
+		return  bio(bio) + firstName(fName) + lastName(lname) + phone(phone);		
 	}
 	
-	public static String id(String id) {
-		String errorMessage = "";
-		try {
-			int memberID = Integer.parseInt(id);
-		} catch (NumberFormatException nfe) {
-			errorMessage +="Author ID must be a number!\n";
-		}
-        if (id.length() > 10) {
-        	errorMessage +="Author ID must not be greater than 10 characters\n";  
-		}
-        return errorMessage;
-	}
+//	public static String id(String id) {
+//		String errorMessage = "";
+//		try {
+//			int memberID = Integer.parseInt(id);
+//		} catch (NumberFormatException nfe) {
+//			errorMessage +="Author ID must be a number!\n";
+//		}
+//        if (id.length() > 10) {
+//        	errorMessage +="Author ID must not be greater than 10 characters\n";  
+//		}
+//        return errorMessage;
+//	}
 	
 	public static String bio(String bio) {
 		String errorMessage = "";
-        if (!bio.matches("[0-9a-zA-Z]*")) {
-        	errorMessage +="Bio field may not contain spaces or characters other than a-z, A-Z\n";  
-		}
+//        if (!bio.matches("[0-9a-zA-Z]*")) {
+//        	errorMessage +="Bio field may not contain spaces or characters other than a-z, A-Z\n";  
+//		}
         if (bio.length() > 254) {
         	errorMessage +="Bio field must not be greater than 254 characters\n";  
 		}
